d1b3de61ec0255807b52017655adc278ca16384a
mandy <535286768@qq.com>
2020-07-03 00:57:12 +0800

    添加 icon 和 iconPosition属性

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12437,7 +12437,16 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  props: ["icon", "iconPosition"]
+};
 exports.default = _default;
         var $3f76be = exports.default || module.exports;
       
@@ -12448,10 +12457,26 @@ exports.default = _default;
         /* template */
         Object.assign($3f76be, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "g-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12528,7 +12553,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61215" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65220" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,17 +1,30 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {};
+export default {
+  props: ["icon", "iconPosition"]
+};
 </script>
 <style lang="scss">
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 1em;
+  padding: 0 0.5em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle; //使按钮对齐
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -22,6 +35,23 @@ export default {};
 
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+    }
+    > .icon {
+      order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
+    }
   }
 }
 </style>
